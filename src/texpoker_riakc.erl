eefdeee9a7754f1818a4f0308b8ef645009f584e
ronalfei <ronalfei@qq.com>
2011-09-27 21:48:25 +0800

    add riak client module

---
+++ src/texpoker_riakc.erl
@@ -0,0 +1,29 @@
+-module(texpoker_riakc).
+
+-include("texpoker.hrl").
+
+-compile(export_all).
+
+
+connect() -> 
+	{ok, Pid} = riakc_pb_socket:start_link(?RIAK_HOST, ?RIAK_PORT),
+	Pid.
+
+new(Key, Value) -> 
+	Object = riakc_obj:new(?BUCKET, Key, Value),
+	Object.
+
+save(Pid,Object) ->
+	 riakc_pb_socket:put(Pid,Object).
+
+stop(Pid) ->
+	 riakc_pb_socket:stop(Pid).
+
+
+
+
+set(Key,Value) -> 
+	Object = new(Key,Value),
+	Pid = connect(),
+	save(Pid, Object),
+	stop(Pid).
